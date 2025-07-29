a94865f28a8261a330294e693ec3db53a1fb0005
Kamal Darchini <kamaldarchini@Kamals-MacBook-Air.local>
2025-07-28 17:52:45 -0700

    updated dash name

--- investments.py
+++ investments.py
@@ -2,5 +2,4 @@
 import streamlit as st
 
 st.set_page_config(layout="wide")
-st.title("Welcome to a set of handy reports")
-st.title("to decide on investments.")
+st.title("Investment Insights Dashboard")
